8a03ff53356cd069d5a3f27104ab5b3c71dfefed
Kuba Król <kuba.krol.ldz@gmail.com>
2023-12-02 21:07:18 +0100

    Aktualizacja Strony InternetoweJ
    - Dodanie ng container i template w projects
    - Dodanie splide-slide zdjęć w projekcie
    - Usunięcie chwilowo footera
    - Dodanie Dwóch Zdjęć Projektu
    - Podłączenie do styles, scripts

--- src/app/projects/projects.component.spec.ts
+++ src/app/projects/projects.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProjectsComponent } from './projects.component';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 describe('ProjectsComponent', () => {
   let component: ProjectsComponent;
