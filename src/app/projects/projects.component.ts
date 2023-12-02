0b42c44a6a682d62f2fc78f02e4e83e90b1a92ae
Kuba Król <kuba.krol.ldz@gmail.com>
2023-12-02 15:51:05 +0100

    #

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Splide }from '@splidejs/splide';
 @Component({
   selector: 'app-projects',
   standalone: true,
@@ -8,16 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+    ngOnInit(){
+      new Splide('.splide').mount();
+    }
     // Zmienna ViewProject jest początku ustawiona na null,
     // ponieważ jeszcze nie przypisano żadnego projektu do wyświetlenia.
     ViewProject: string | null = null;
-
     // Funkcja Pokażania Szczegołówych informacji o projekcie
     ViewOnProject(project: string): void {
       this.ViewProject = project;
     }
-    
     // Funkcja Ukrycia Szczegołówych informacji o projekcie
     ViewOffProject(): void{
       this.ViewProject = null;
