927fb72698a285d0e9aeab6c1310a1400848dad6
Kuba Król <kuba.krol.ldz@gmail.com>
2024-05-25 17:53:55 +0200

    poprawki

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
         
         <Navbar/>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
 
