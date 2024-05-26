fe4eb8e014fe7f370423b7611304bbab97d0b051
Kuba Król <kuba.krol.ldz@gmail.com>
2024-05-26 14:01:38 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio",
+  base: '/portfolio/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
