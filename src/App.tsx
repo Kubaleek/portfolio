eef5476457e6caac4aaa3e6f52003496042ef0e8
Kuba Król <kuba.krol.ldz@gmail.com>
2024-05-26 13:46:22 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from './components/navbar/navbar'
 import Footer from './components/footer/footer'
 import { useEffect }  from 'react'
 import Home from './components/pages/Home'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom'
 import Projects from './components/pages/projects'
 
 function showPreLoader(){
@@ -37,12 +37,10 @@ function App() {
     <>
     
         <Navbar/>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-          </Routes>
-        </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
 
         <div className="side_left d-none d-lg-block">
           <div className="side">
