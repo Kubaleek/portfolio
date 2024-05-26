c184e3a999b04128023bc7df46ae3ea0e8069557
Kuba Król <kuba.krol.ldz@gmail.com>
2024-05-26 14:11:03 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,40 @@ import './App.css'
 import Navbar from './components/navbar/navbar'
 import Footer from './components/footer/footer'
 import Home from './components/pages/Home'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {Route, Routes } from 'react-router-dom'
 import Projects from './components/pages/projects'
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
+    
+        <Navbar/>
+        
         <Routes>
+          <Route index element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
-      </Router>
-      <div className="side_left d-none d-lg-block">
-        <div className="side">
-          <a href="https://github.com/Kubaleek" target='_blank' title="Github">
-            <i className="bi bi-github"></i>
-          </a>
-          <a href="https://www.instagram.com/kubalekweb/" target='_blank' title="Instagram">
-            <i className="bi bi-instagram"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/kuba-kr%C3%B3l-9b4946274/" target='_blank' title="Linkedin">
-            <i className="bi bi-linkedin mb-3"></i>
-          </a>
+
+        <div className="side_left d-none d-lg-block">
+          <div className="side">
+              <a href="https://github.com/Kubaleek" target='_blank' title="Github">
+                <i className="bi bi-github"></i>
+              </a>
+              <a href="https://www.instagram.com/kubalekweb/" target='_blank' title="Instagram">
+                <i className="bi bi-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/kuba-kr%C3%B3l-9b4946274/"  target='_blank'  title="Linkedin">
+                <i className="bi bi-linkedin mb-3"></i>
+              </a>
+          </div>
         </div>
-      </div>
-      <div className="side_right d-none d-lg-block">
-        <div className="side">
-          <a href="mailto:kuba.krol.ldz@gmail.com">kuba.krol.ldz@gmail.com</a>
+        <div className="side_right d-none d-lg-block">
+          <div className="side">
+              <a href="mailto:kuba.krol.ldz@gmail.com">kuba.krol.ldz@gmail.com</a>
+          </div>
         </div>
-      </div>
-      <Footer />
+        <Footer/>
     </>
   )
 }
