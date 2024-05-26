3701d87a62c69434404a6cd111a4ba4c2eafbbf3
Kuba Król <kuba.krol.ldz@gmail.com>
2024-05-26 12:56:11 +0200

    poprawki dalsze

--- src/App.tsx
+++ src/App.tsx
@@ -34,40 +34,35 @@ function App() {
     }
   })
   return (
-    <>
-    
-        <Navbar/>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-
-        <div className="side_left d-none d-lg-block">
-          <div className="side">
-              <a href="https://github.com/Kubaleek" target='_blank' title="Github">
-                <i className="bi bi-github"></i>
-              </a>
-              <a href="https://www.instagram.com/kubalekweb/" target='_blank' title="Instagram">
-                <i className="bi bi-instagram"></i>
-              </a>
-              <a href="https://www.linkedin.com/in/kuba-kr%C3%B3l-9b4946274/"  target='_blank'  title="Linkedin">
-                <i className="bi bi-linkedin mb-3"></i>
-              </a>
-          </div>
-        </div>
-        <div className="side_right d-none d-lg-block">
-          <div className="side">
-              <a href="mailto:kuba.krol.ldz@gmail.com">kuba.krol.ldz@gmail.com</a>
-          </div>
-        </div>
-        <Footer/>
-      </>
-    </BrowserRouter>
-        <Footer/>
-    </>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <>
+            <Navbar/>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects" element={<Projects />} />
+            </Routes>
+      
+            <div className="side_left d-none d-lg-block">
+              <div className="side">
+                  <a href="https://github.com/Kubaleek" target='_blank' title="Github">
+                    <i className="bi bi-github"></i>
+                  </a>
+                  <a href="https://www.instagram.com/kubalekweb/" target='_blank' title="Instagram">
+                    <i className="bi bi-instagram"></i>
+                  </a>
+                  <a href="https://www.linkedin.com/in/kuba-kr%C3%B3l-9b4946274/"  target='_blank'  title="Linkedin">
+                    <i className="bi bi-linkedin mb-3"></i>
+                  </a>
+              </div>
+            </div>
+            <div className="side_right d-none d-lg-block">
+              <div className="side">
+                  <a href="mailto:kuba.krol.ldz@gmail.com">kuba.krol.ldz@gmail.com</a>
+              </div>
+            </div>
+            <Footer/>
+          </>
+      </BrowserRouter>
   )
 }
 
