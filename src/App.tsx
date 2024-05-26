7e6b665de49f7e5224b09ceef1f1d19994d29f3e
Kuba Król <kuba.krol.ldz@gmail.com>
2024-05-26 13:00:57 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from './components/navbar/navbar'
 import Footer from './components/footer/footer'
 import { useEffect }  from 'react'
 import Home from './components/pages/Home'
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Projects from './components/pages/projects'
 
 function showPreLoader(){
@@ -34,35 +34,34 @@ function App() {
     }
   })
   return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <>
-            <Navbar/>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/projects" element={<Projects />} />
-            </Routes>
-      
-            <div className="side_left d-none d-lg-block">
-              <div className="side">
-                  <a href="https://github.com/Kubaleek" target='_blank' title="Github">
-                    <i className="bi bi-github"></i>
-                  </a>
-                  <a href="https://www.instagram.com/kubalekweb/" target='_blank' title="Instagram">
-                    <i className="bi bi-instagram"></i>
-                  </a>
-                  <a href="https://www.linkedin.com/in/kuba-kr%C3%B3l-9b4946274/"  target='_blank'  title="Linkedin">
-                    <i className="bi bi-linkedin mb-3"></i>
-                  </a>
-              </div>
-            </div>
-            <div className="side_right d-none d-lg-block">
-              <div className="side">
-                  <a href="mailto:kuba.krol.ldz@gmail.com">kuba.krol.ldz@gmail.com</a>
-              </div>
-            </div>
-            <Footer/>
-          </>
-      </BrowserRouter>
+    <>
+    
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+
+        <div className="side_left d-none d-lg-block">
+          <div className="side">
+              <a href="https://github.com/Kubaleek" target='_blank' title="Github">
+                <i className="bi bi-github"></i>
+              </a>
+              <a href="https://www.instagram.com/kubalekweb/" target='_blank' title="Instagram">
+                <i className="bi bi-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/kuba-kr%C3%B3l-9b4946274/"  target='_blank'  title="Linkedin">
+                <i className="bi bi-linkedin mb-3"></i>
+              </a>
+          </div>
+        </div>
+        <div className="side_right d-none d-lg-block">
+          <div className="side">
+              <a href="mailto:kuba.krol.ldz@gmail.com">kuba.krol.ldz@gmail.com</a>
+          </div>
+        </div>
+        <Footer/>
+    </>
   )
 }
 
